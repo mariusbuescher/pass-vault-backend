2aae158aa95c7fbeea703214bcf8757483d349c8
Marius Büscher <marius.buescher@gmx.de>
2018-12-07 22:07:36 +0100

    Fix migration for public key id

--- resources/db/migration/postgres/V1_1_0__Add_Public_Key.sql
+++ resources/db/migration/postgres/V1_1_0__Add_Public_Key.sql
@@ -1,7 +1,7 @@
 ALTER TABLE auth_token ADD CONSTRAINT fk_auth_token_auth_user FOREIGN KEY (user_id) REFERENCES auth_user(user_id);
 
 CREATE TABLE crypt_pub_key (
-    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    id UUID NOT NULL PRIMARY KEY,
     public_key TEXT NOT NULL,
     added_at TIMESTAMP NOT NULL,
     user_id VARCHAR(255) NOT NULL,
