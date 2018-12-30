f39c70fb0705ab231fff612b9a4c114ebfa723d1
Marius Büscher <marius.buescher@gmx.de>
2018-12-30 20:17:56 +0100

    Extend password dto with id property

--- src/main/kotlin/dto/PasswordDto.kt
+++ src/main/kotlin/dto/PasswordDto.kt
@@ -1,6 +1,9 @@
 package dto
 
+import java.util.*
+
 data class PasswordDto(
+        val id: UUID? = null,
         val name: String? = null,
         val domain: String? = null,
         val account: String? = null
